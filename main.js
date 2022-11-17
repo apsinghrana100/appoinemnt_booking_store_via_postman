17d7cc5a0964c2a97a6ef63c0b16b3bbe156a83c
apsinghrana100 <apsinghrana100@gmail.com>
2022-11-18 00:12:58 +0530

    show_user_detail

--- main.js
+++ main.js
@@ -29,84 +29,124 @@ function storedata(e){
             name,
             email
         };
-        axios.post("https://crudcrud.com/api/929af3c6fd5242fc8679ea5a6e37ad7b/bookapinmnetdetail",obj).then((res)=>{
-            console.log(res);
-            //showOutput(res)
-          }).catch((err)=>{
-            console.log(err);
-          })
-        console.log("suceess");
+        // axios.post("https://crudcrud.com/api/f926f97655744c3a8a92d538bddec55d/bookapinmnetdetail",obj).then((res)=>{
+        //     console.log(res);
+        //     //showOutput(res)
+        //   }).catch((err)=>{
+        //     console.log(err);
+        //   })
+        // console.log("suceess");
+
+        axios({
+          method:'post',
+          url:"https://crudcrud.com/api/f926f97655744c3a8a92d538bddec55d/bookapinmnetdetail",
+          data:obj
+           }).then((res)=>{
+              console.log(res);
+            
+              //showOutput(res)
+            }).catch((err)=>{
+              console.log(err);
+            })
     
 
-  displaydata(obj);
-     cleardata();
-}
+            displaydata(obj);
+             cleardata();
+      }
 }
 
 ////////////////////for window refresh///////////////////////////
 
 window.addEventListener('DOMContentLoaded',(event)=>{
     console.log("i am window");
-    Object.keys(axios.get("https://crudcrud.com/api/929af3c6fd5242fc8679ea5a6e37ad7b/bookapinmnetdetail/id")).forEach(key => {
-                const user = JSON.parse(axios.get("https://crudcrud.com/api/929af3c6fd5242fc8679ea5a6e37ad7b/bookapinmnetdetail"))
-                displaydata(user)
-                console.log(`name ${user.name}`);
-})});
+    axios.get("https://crudcrud.com/api/f926f97655744c3a8a92d538bddec55d/bookapinmnetdetail")
+    .then((Response)=>{
+      console.log(Response);
+      for(var i=0;i<Response.data.length;i++)
+      {
+        displaydata(Response.data[i]);
+      }
+    }).catch((error)=>{
+      console.log(error)
+    });
+
+                
+});
 
 
 //////////////////for display///////////////////////////////////////
 
-// function  displaydata(obj){
+function  displaydata(obj){
     
-//     const li=`<li id=${obj.email}>${obj.name}-${obj.email}
-//     <button onclick=deletedata('${obj.email}')>delete</button>
-//     <button onclick=editdata('${obj.name}','${obj.email}')>edit</button></li>`;
-//     ul.innerHTML=ul.innerHTML+li;
+    const li=`<li id=${obj._id}>${obj.name}-${obj.email}
+    <button onclick=deletedata('${obj._id}')>delete</button>
+    <button onclick=editdata('${obj._id}','${obj.name}','${obj.email}')>edit</button></li>`;
+    ul.innerHTML=ul.innerHTML+li;
 
-// }
-
-
-
-// //////////////////////deletedata///////////////////////////////////
-// function deletedata(deleteid)
-// {
-//     localStorage.removeItem(deleteid);
-//     removedata(deleteid);
-// }
+}
 
 
 
-// ////////////////remove the data form the screen////////////////////////////
+//////////////////////deletedata///////////////////////////////////
+function deletedata(deleteid)
+{
+   // localStorage.removeItem(deleteid);
 
-// function removedata(deleteid)
-// {
-//     let parentNode=document.getElementById('users');
-//     let elementnode=document.getElementById(deleteid);
-//     parentNode.removeChild(elementnode);
+    axios.delete(`https://crudcrud.com/api/f926f97655744c3a8a92d538bddec55d/bookapinmnetdetail/${deleteid}`)
+    .then(()=>{
+      removedata(deleteid);
+    }).catch((err)=>{
+        console.log(err);
+    })
+   
 
-// }
-
-
-
-
-
-// //////////////////////////////////updata_data////////////////////
-// function editdata(name,emailid){
-//     const name1=document.getElementById('name').value=name;
-//     const email=document.getElementById('email').value=emailid;
-
-//     sumbmit.value='UPDATE';
-//     sumbmit.style.background='GREEN';
-
-//     deletedata(email);
-// }
+}
 
 
-// ////////////////////////clear_data//////////////////////////////////
+
+////////////////remove the data form the screen////////////////////////////
+
+function removedata(deleteid)
+{
+    let parentNode=document.getElementById('users');
+    let elementnode=document.getElementById(deleteid);
+    parentNode.removeChild(elementnode);
+
+}
 
 
-// function cleardata(){
-// const name=document.getElementById('name').value='';
-// const email=document.getElementById('email').value='';
 
-// }
+
+
+//////////////////////////////////updata_data////////////////////
+function editdata(id,name,emailid){
+  const name1=document.getElementById('name').value=name;
+    const email=document.getElementById('email').value=emailid;
+  axios({
+      method:'put',
+      url:`https://crudcrud.com/api/f926f97655744c3a8a92d538bddec55d/bookapinmnetdetail/${id}`,
+        data:{
+          "name":name1,
+          "email":email
+        }
+  }).then((Response)=>{
+    console.log(respone);
+  }).catch((err)=>{
+    console.log(err);
+  });
+    
+
+    sumbmit.value='UPDATE';
+    sumbmit.style.background='GREEN';
+
+}
+
+
+////////////////////////clear_data//////////////////////////////////
+
+
+function cleardata(){
+const name=document.getElementById('name').value='';
+const email=document.getElementById('email').value='';
+
+}
